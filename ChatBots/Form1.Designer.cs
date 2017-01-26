03190086b79690b70c61d19c25fb3f58e609c218
yuranicorp <yuranicorp@gmail.com>
2017-01-26 08:17:16 +0700

    Add files via upload

---
+++ ChatBots/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ChatBots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.kolomChat = new System.Windows.Forms.TextBox();
+            this.richChat = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.jawaban = new System.Windows.Forms.TextBox();
+            this.pertanyaan = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(378, 332);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnSend);
+            this.tabPage1.Controls.Add(this.kolomChat);
+            this.tabPage1.Controls.Add(this.richChat);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(370, 306);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Chat";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnSend.Location = new System.Drawing.Point(290, 277);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(77, 26);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "&Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // kolomChat
+            // 
+            this.kolomChat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.kolomChat.Location = new System.Drawing.Point(8, 280);
+            this.kolomChat.Name = "kolomChat";
+            this.kolomChat.Size = new System.Drawing.Size(279, 20);
+            this.kolomChat.TabIndex = 1;
+            this.kolomChat.TextChanged += new System.EventHandler(this.kolomChat_TextChanged);
+            this.kolomChat.KeyDown += new System.Windows.Forms.KeyEventHandler(this.kolomChat_KeyDown);
+            // 
+            // richChat
+            // 
+            this.richChat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richChat.FormattingEnabled = true;
+            this.richChat.Location = new System.Drawing.Point(0, 0);
+            this.richChat.Name = "richChat";
+            this.richChat.Size = new System.Drawing.Size(373, 264);
+            this.richChat.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.jawaban);
+            this.tabPage2.Controls.Add(this.pertanyaan);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(370, 306);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Teach";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "&Jawaban";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "&Pertanyaan";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Teach";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // jawaban
+            // 
+            this.jawaban.Location = new System.Drawing.Point(8, 96);
+            this.jawaban.Multiline = true;
+            this.jawaban.Name = "jawaban";
+            this.jawaban.Size = new System.Drawing.Size(359, 147);
+            this.jawaban.TabIndex = 1;
+            // 
+            // pertanyaan
+            // 
+            this.pertanyaan.Location = new System.Drawing.Point(8, 42);
+            this.pertanyaan.Name = "pertanyaan";
+            this.pertanyaan.Size = new System.Drawing.Size(322, 20);
+            this.pertanyaan.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 332);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Tag = "XC";
+            this.Text = "Bot Talk";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox kolomChat;
+        private System.Windows.Forms.ListBox richChat;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox jawaban;
+        private System.Windows.Forms.TextBox pertanyaan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ChatBots/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ChatBots
+{
+    /// <summary>
+    /// SOURCE CODE BY YUSRIL TAKEUCHI
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public string pesan;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string[] input = new string[5];
+        public string[] output = new string[5];
+        public string pertanyaans;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pertanyaans = pertanyaan.Text;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == null)
+                {
+                    if (output[i] == null)
+                    {
+                        input[i] = pertanyaan.Text;
+                        output[i] = jawaban.Text;
+                        MessageBox.Show("Menambahkan kata baru..." + "\n" + "Pertanyaan: " + input[i] + "\n" + "Jawaban: " + output[i], "New Word", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+
+                    }
+                    
+
+                }
+                else
+                {
+                    if (input[i].ToLower().Contains(pertanyaans.ToLower()))
+                    {
+                        output[i] = output[i] + " " + jawaban.Text;
+                        MessageBox.Show("Menambahkan kata baru..." + "\n" + "Pertanyaan: " + input[i] + "\n" + "Jawaban: " + output[i], "New Word", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            pesan = kolomChat.Text;
+            if (kolomChat.Text != string.Empty)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    
+                    if (input[i] != null)
+                    {
+                        if (output[i] != null)
+                        {
+                            if (input[i].ToLower().Contains(pesan.ToLower()))
+                            {
+                                richChat.Items.Add("Kamu: " + kolomChat.Text);
+                                kolomChat.Text = string.Empty;
+                                richChat.Items.Add("Bot: " + output[i]);
+                                break;
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+
+
+            }
+        }
+        private void kolomChat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kolomChat_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSend.PerformClick();
+            }
+        }
+    }
+}
